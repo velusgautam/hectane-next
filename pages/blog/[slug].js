45aab2137251121bc1c6185dcab05636bd52c69c
Velu Gautam <velu.gautam@comeon.com>
2020-06-22 16:19:23 +0530

    Updated meta tag

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,4 +1,5 @@
 import { useRouter } from 'next/router';
+import Head from 'next/head';
 import useSWR from 'swr';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
@@ -14,6 +15,33 @@ export default function Blog() {
   const post = data;
   return (
     <div>
+      <Head>
+        <title>{post.title}</title>
+        <meta name="keywords" content={post.tags.join(', ')} />
+        <meta name="description" content={post.subTitle} />
+
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta name="twitter:site" content="@_hectane" />
+        <meta name="twitter:creator" content="@velusgautam" />
+        <meta name="twitter:title" content={post.title} />
+        <meta name="twitter:description" content={post.subTitle} />
+        <meta
+          name="twitter:image"
+          content={`https://assets.hectane.com/${post.route}/title.jpg`}
+        />
+
+        <meta
+          property="og:url"
+          content={`https://hectane.com/blog/${post.route}`}
+        />
+        <meta property="og:type" content="article" />
+        <meta property="og:title" content={post.title} />
+        <meta property="og:description" content={post.subTitle} />
+        <meta
+          property="og:image"
+          content={`https://assets.hectane.com/${post.route}/title.jpg`}
+        />
+      </Head>
       <div className="content">
         <h1 className="post--title">{post.title}</h1>
         <h4 className="post--sub-title">{post.subTitle}</h4>
