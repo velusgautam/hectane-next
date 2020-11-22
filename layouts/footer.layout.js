b67a33fdc06539152823011708b28474a1ce9bee
Velu Gautam <velu.gautam@comeon.com>
2020-11-22 22:54:22 +0100

    Added RSS footer link

--- layouts/footer.layout.js
+++ layouts/footer.layout.js
@@ -1,23 +1,31 @@
-import Link from 'next/link';
-import { footerRoutes } from './routes';
+import Link from 'next/link'
+import { footerRoutes } from './routes'
 
 const Footer = () => {
   return (
     <footer className={`margin-auto container footer-container`}>
       <div>
-        {footerRoutes.map((route) => {
+        {footerRoutes.map(route => {
           return (
             <Link href="/[slug]" as={`${route.path}`} key={route.path}>
               <a className="footer-links">{route.name}</a>
             </Link>
-          );
+          )
         })}
+        <a
+          href="/feed.xml"
+          target="_blank"
+          rel="noopener noreferrer"
+          className="footer-links"
+        >
+          RSS
+        </a>
       </div>
       <div className="footer-text">
         {`Hectane All Rights Reserved. Copyright 2018 - ${new Date().getFullYear()}`}
       </div>
     </footer>
-  );
-};
+  )
+}
 
-export default Footer;
+export default Footer
